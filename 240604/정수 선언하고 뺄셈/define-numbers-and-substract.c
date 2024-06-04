498159bd74f9ae2f2228c95c3b098dd4e646b647
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-04 11:33:20 +0200

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240604/정수 선언하고 뺄셈/define-numbers-and-substract.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num1 = 97;
+    int num2 = 13;
+    int result = num1-num2;
+    printf("%d - %d = %d", num1, num2, result);
+    return 0;
+}
