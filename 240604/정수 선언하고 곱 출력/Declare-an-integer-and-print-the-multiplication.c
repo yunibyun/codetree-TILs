f13b31a70885979057c7beb908e443c72b45e13d
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-04 11:35:59 +0200

    Title: 정수 선언하고 곱 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240604/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num1 = 26;
+    int num2 = 5;
+    int result = num1 * num2;
+    printf("%d * %d = %d", num1, num2, result);
+    return 0;
+}
