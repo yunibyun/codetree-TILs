bca1bb7593d2f5a3fc660aeca0ea8e9fc8de174e
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-04 11:37:18 +0200

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240604/세 정수형 변수 선언/declaration-of-three-natural-numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num1 = 7;
+    int num2 = 23;
+    int num3 = 30;
+    printf("%d + %d = %d", num1, num2, num3);
+    return 0;
+}
