40c7c32fbcb701c598a0970ef7d91d8824753505
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-06 10:43:03 +0200

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240606/음수 구별하기/separate-negative-number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d", &a);
+    printf("%d", a);
+    if(a < 0) {
+        printf("\nminus", a);
+    };
+
+    return 0;
+}
