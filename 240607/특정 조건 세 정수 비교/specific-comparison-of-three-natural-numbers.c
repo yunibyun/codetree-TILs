8890edd85890d6e07f4cab06f0d37873f3ccddf6
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-06 19:01:14 +0200

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240607/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.c
+++ 240607/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.c
@@ -5,7 +5,7 @@ int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
     
-    printf("%d ", a < b && a < c);
+    printf("%d ", a <= b && a <= c);
     printf("%d", a == b && a == c);
     return 0;
 }
