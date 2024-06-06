6b1925488fe655a8db1787245d8bec41f21899f3
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-06 11:21:57 +0200

    Title: 숫자의 조건 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240606/숫자의 조건 여부/number's-condition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d", &a);
+    if (a>=113) {
+        printf("%d", 1);
+    } else {
+        printf("0");
+    }
+    return 0;
+}
