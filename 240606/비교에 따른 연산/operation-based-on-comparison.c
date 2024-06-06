7bc3a078a6b2ffebfc0ef5a4dc4299ebef38cacf
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-06 11:24:26 +0200

    Title: 비교에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240606/비교에 따른 연산/operation-based-on-comparison.c
+++ 240606/비교에 따른 연산/operation-based-on-comparison.c
@@ -5,10 +5,10 @@ int main() {
     int a, b;
     scanf("%d %d", &a, &b);
 
-    if (a>b) {
+    if (a > b) {
         printf("%d", a * b);
     } else {
-        printf("%d", a/b);
+        printf("%d", b/a);
     }
     return 0;
 }
