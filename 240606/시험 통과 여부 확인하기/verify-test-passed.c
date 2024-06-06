0c827d73ee637869676aa34f086dcbbbc1e288ce
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-06 11:19:39 +0200

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240606/시험 통과 여부 확인하기/verify-test-passed.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+    if ( n >= 80 ) {
+        printf("pass");
+    } else {
+        printf("\n%d more score", 80 - n);
+    }
+    return 0;
+}
