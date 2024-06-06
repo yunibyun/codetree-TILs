771ed27cb010d576fceb1040f3f37a91d218e2ef
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-06 18:00:12 +0200

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240607/3 또는 5의 배수/multiples-of-3-or-5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d", &a);
+
+    if ( a % 3 == 0 ){
+        printf ("YES/n");
+    } else {
+        printf("NO/n");
+    }
+
+    if ( a % 5 == 0 ){
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+    return 0;
+}
