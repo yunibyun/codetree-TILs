b83cb1f829a86ae91be6bb98cf3d9a31bc3ccf6c
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-06 17:58:18 +0200

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240607/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
+++ 240607/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 변수 선언
     int a, b;
-    scanf("%d %d", &a, &b);
 
-    if ( a < b ){
-        printf("1 0");
-    } else if ( a == b ){
-        printf("0 1");
-    }
+	// 입력
+	scanf("%d %d", &a, &b);
+    
+    // 출력
+	if(a < b)
+		printf("1 ");
+	else
+		printf("0 ");
+	
+	if(a == b)
+		printf("1");
+	else
+		printf("0");
+
     return 0;
 }
