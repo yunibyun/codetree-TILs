fc9965e078e357d69612484b4374a6e5b5da8f31
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-06 18:36:10 +0200

    Title: 4가지 관계연산자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240607/4가지 관계연산자/four-relational-operators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d", a >= b);
+    printf("\n%d", a > b);
+    printf("\n%d", b >= a);
+    printf("\n%d", b > a);
+    return 0;
+}
