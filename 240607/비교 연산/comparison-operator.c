c89e2331148bb1d654f5eff648e00ea6f0bf3cd8
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-06 18:34:25 +0200

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240607/비교 연산/comparison-operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d", a >= b);
+    printf("\n%d", a > b);
+    printf("\n%d", b >= a);
+    printf("\n%d", b > a);
+    printf("\n%d", a == b);
+    printf("\n%d", a!=b);
+    return 0;
+}
