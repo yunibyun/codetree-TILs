82da735f9875c3e62f2199ff656659efa6a5d622
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-07 20:50:43 +0200

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240608/n까지 숫자 출력/print-1-to-n.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int i = 1;
+    scanf("%d", &n);
+
+    while ( i <= n ){
+        printf("%d ", i);
+        i += 1;
+    }
+    return 0;
+}
