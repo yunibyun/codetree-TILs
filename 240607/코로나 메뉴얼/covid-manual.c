ed4e13a066267070e0b046dd950702e2e916fbb1
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-07 10:53:46 +0200

    Title: 코로나 메뉴얼, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240607/코로나 메뉴얼/covid-manual.c
+++ 240607/코로나 메뉴얼/covid-manual.c
@@ -2,7 +2,6 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    char a = 'A', b = 'B', c = 'C', d = 'D';
     char symptoms_a, symptoms_b, symptoms_c;
     int temp_a, temp_b, temp_c;
 
@@ -10,14 +9,14 @@ int main() {
     scanf("%c %d\n", &symptoms_b, &temp_b);
     scanf("%c %d", &symptoms_c, &temp_c);
     
-    if (symptoms_a = 'Y' && temp_a >= 37){
-        if ((symptoms_b = 'Y' && temp_b >= 37) || (symptoms_c = 'Y' && temp_c >= 37)){
+    if ((symptoms_a = 'Y') && (temp_a >= 37)){
+        if (((symptoms_b = 'Y') && (temp_b >= 37)) || ((symptoms_c = 'Y') && (temp_c >= 37))){
             printf ("E");
         } else {
             printf ("N");
         }
-    } else if (symptoms_b = 'Y' && temp_b >= 37){
-        if (symptoms_c = 'Y' && temp_c >= 37){
+    } else if ((symptoms_b = 'Y') && (temp_b >= 37)){
+        if ((symptoms_c = 'Y') && (temp_c >= 37)){
             printf ("E");
         } else {
             printf ("N");
