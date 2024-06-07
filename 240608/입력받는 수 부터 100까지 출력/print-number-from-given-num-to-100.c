03604fe64eaf138a3aecfc23e039e769bb943990
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-07 19:51:55 +0200

    Title: 입력받는 수 부터 100까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240608/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int i;
+    scanf("%d", &i);
+
+    for (i += 0 ; i <= 100 ; i += 1){
+        printf("%d ", i);
+    }
+    return 0;
+}
