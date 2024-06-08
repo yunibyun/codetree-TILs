d64ebefa2148343baa0fa4a7c15c01a12416fdd8
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-08 17:43:57 +0200

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240609/n번 더하기/add-n-times.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, n;
+    scanf("%d %d", &a, &n);
+
+    for ( int i = 1; i <= n ; i++ ){
+        a += n;
+        printf("%d\n", a);
+    }
+    return 0;
+}
