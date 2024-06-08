b8b008f073bec02d5946a32f0fad8b82f90a2d23
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-08 09:57:23 +0200

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240608/문자 출력하기/print-text.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char a;
+    scanf("%c", &a);
+
+    for ( int i = 1; i <= 8 ; i++ ){
+        printf("%c", a);
+    }
+    return 0;
+}
