8407b7edbe64d38fc4fe0738babaf16193b7ddd5
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-09 15:08:01 +0200

    Title: 369 게임, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240609/369 게임/369-game.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+    int i = 1;
+
+    while (i <= n){
+        if ( i % 3 == 0 ){
+            printf("0 ");
+        } else if ( i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+            printf("0 ");
+        } else {
+            printf("%d ", i);
+        }
+        i++;
+    }
+    return 0;
+}
