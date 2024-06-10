67f81df42871b5a2906700afe4386354828f1cd5
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-10 18:48:43 +0200

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240611/특정 숫자 맞추기/catching-specific-number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    
+    int n;
+
+    // 주어지는 정수값이 25가 나올때까지 계속 입력을 받기 위해 무한루프 사용
+    while(1){
+        scanf("%d\n", &n);
+        if (25 < n){
+            printf("Lower\n");
+            continue;
+        } else if (n < 25) {
+            printf("Higher\n");
+            continue;
+        } else if ( n == 25 ) {
+            printf("Good");
+            break;
+        }
+    }
+    return 0;
+}
