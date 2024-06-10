d5ad683ca18f10a6ba6b0460d7e47e495d624902
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-10 13:35:45 +0200

    Title: 범위 안의 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240610/범위 안의 수 2/number-in-range-2.c
+++ 240610/범위 안의 수 2/number-in-range-2.c
@@ -4,8 +4,8 @@ int main() {
     // 여기에 코드를 작성해주세요.
     // 주어질 정수 a 선언
     int a;
-    // 변수의 합을 저장할 sum 변수 선언, 항상 0으로 초기화
-    int sum = 0;
+    // 변수의 합을 저장할 sum 변수 선언, 변수의 갯수를 셀 cnt 변수 선언; 항상 0으로 초기화
+    int sum = 0, cnt = 0;
     // 평균을 저장할 average 변수 선언;
     double average;
 
@@ -13,12 +13,15 @@ int main() {
     for (int i = 1; i <= 10; i++){
         // 변수 입력받기
         scanf("%d", &a);
-        // 입력받은 변수 더하기
-        sum += a;
+        //  0이상 200이하의 정수들의 경우 cnt 증가시키고 값 더하기
+        if ( 0 <= a && a <= 200 ){
+            sum += a;
+            cnt ++;
+        }
     }
 
     // average 계산
-    average = (double) sum / 10;
+    average = (double) sum / cnt;
     // 합과 평균 출력
     printf("%d %.1f", sum, average);
 
