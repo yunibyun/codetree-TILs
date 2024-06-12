bb49fbf6eac50575be48185bd08bc10d0c082338
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-12 09:25:18 +0200

    Title: ab 사이에 있는 c, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240612/ab 사이에 있는 c/c-between-a-and-b.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+// boolean data type을 쓰기 위해서는 stdbool.h를 넣어주어야!
+int main() {
+    // 여기에 코드를 작성해주세요
+
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    // c의 배수 유무를 알려줄 satisfied 변수 boolean type으로 선언
+    bool satisfied = false;
+
+    // a에서 b까지 iteration을 통해서 c의 배수가 있는지 확인한다
+    // c의 배수가 존재할 경우 satisfied의 값을 true로 바꾼다
+    for ( int i = a; i <= b; i++ ){
+        if ( i % c == 0 ){
+            satisfied = true;
+        }
+    }
+
+    //
+    if ( satisfied = true ) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+    return 0;
+}
