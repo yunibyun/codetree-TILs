6bdc4994ecc3c2e304b82ba88fbe0104502c7383
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-14 15:12:57 +0200

    Title: 별표 출력하기 11, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240614/별표 출력하기 11/print-star-11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+    // 총 2n + 1 줄 출력
+    for ( int i = 0 ; i < (2 * n) + 1; i++ ){
+        for ( int j = 0 ; j < (2 * n) + 1; j++ ) {
+            if ( i == 0 || j == 0 || i % 2 == 0 || j % 2 == 0 ){
+                printf("* ");
+            } else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
