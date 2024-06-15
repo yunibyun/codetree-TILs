733a77d4ce924a4f019f25a7e7348760eb35c5c1
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-15 11:28:55 +0200

    Title: 순서쌍 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240615/순서쌍 만들기/making-order-pair.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            printf("(%d,%d) ", n - i, n - j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
