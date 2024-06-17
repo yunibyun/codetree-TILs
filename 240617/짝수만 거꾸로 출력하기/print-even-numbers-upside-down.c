2be9254cae6808bb8a0828f812046cc59e75313f
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-06-17 10:07:45 +0200

    Title: 짝수만 거꾸로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240617/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.c
+++ 240617/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.c
@@ -8,16 +8,18 @@ int main() {
     int arr[100];
 
     for(int i = 0; i < n; i++){
+		// 유저로부터 숫자들을 입력받아요
 	 	scanf("%d ", &arr[i]);
-
-		// 거꾸로 (n-1)번째 index부터 arr 배열을 iteration 해요
-		for (int j = n - 1; j >= 0; j--){
-			// arr[j] 가 짝수일 경우 값을 출력해요
-			if(arr[j] % 2 == 0){
-			printf("%d ", arr[j]);
-			}
-		}
     }
+
+	// 거꾸로 (n-1)번째 index부터 arr 배열을 iteration 해요
+	for (int j = n - 1; j >= 0; j--){
+		// arr[j] 가 짝수일 경우 값을 출력해요
+		if(arr[j] % 2 == 0){
+		printf("%d ", arr[j]);
+		}
+	}
+		
     
     return 0;
 }
