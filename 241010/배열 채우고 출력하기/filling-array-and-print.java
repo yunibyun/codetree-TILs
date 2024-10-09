8f03a62cdbed04e8cd84124bb25c37cf6f35f6cf
Yuna <73470856+yunibyun@users.noreply.github.com>
2024-10-09 20:41:49 +0200

    Title: 배열 채우고 출력하기, Time: 192ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241010/배열 채우고 출력하기/filling-array-and-print.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        char arr[] = new char[10];
+        // 문자 10개 입력받기
+        for (int i = 0; i < 10; i++){
+            arr[i] = sc.next().charAt(0);
+        }
+        // 문자 10개 거꾸로 출력하기
+        for (int i = 9; i >= 0; i--){
+            System.out.print(arr[i]);
+        }
+    }
+}
